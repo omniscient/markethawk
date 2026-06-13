a8286edbdf4787cb22725cc276e6172ca1111d28
MarketHawk Factory <factory@markethawk>
2026-06-13 16:52:56 +0000

    feat(#337): move run_record.py into factory_core package; update test import

--- dark-factory/scripts/factory_core/__init__.py
+++ dark-factory/scripts/factory_core/__init__.py
@@ -15,10 +15,16 @@ from .board import (
     STATUS_BACKLOG,
     STATUS_REFINED,
 )
+from .breaker import get_retry_count, increment_retry, reset_retry, trip_to_blocked
+from .deconflict import resolve_merge_conflicts, tier1, tier2, hard_grep_survivors
+from . import run_record
 
 __all__ = [
     "find_board_item", "set_board_status", "post_or_update_comment",
     "OWNER", "REPO", "PROJECT_NUMBER", "PROJECT_ID", "STATUS_FIELD",
     "STATUS_READY", "STATUS_IN_PROGRESS", "STATUS_IN_REVIEW", "STATUS_BLOCKED",
     "STATUS_DONE", "STATUS_BACKLOG", "STATUS_REFINED",
+    "get_retry_count", "increment_retry", "reset_retry", "trip_to_blocked",
+    "resolve_merge_conflicts", "tier1", "tier2", "hard_grep_survivors",
+    "run_record",
 ]
